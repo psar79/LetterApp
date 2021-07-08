5be2da145f7fbdfc2baa0c1d280dcc8d4c6771f7
psar79 <psar1987@wp.pl>
2021-07-08 23:07:36 +0200

    LET-43 Generate Token(10 digits) and save user Phone Number and refreshToken to user table

--- UserCore/src/test/java/userregister/usercore/utils/RefreshTokenTest.java
+++ UserCore/src/test/java/userregister/usercore/utils/RefreshTokenTest.java
@@ -1,27 +1,27 @@
-//package userregister.usercore.utils;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class RefreshTokenTest {
-//
-//    private RefreshToken refreshToken;
-//
-//    @BeforeEach
-//    void setUp(){
-//        this.refreshToken = new RefreshToken();
-//    }
-//
-//    @Test
-//    public void checkIfWorkingWhenHappyPath() {
-//
-//        //when
-//        String token = refreshToken.tokenCreator();
-//
-//        //then
-//        assertEquals("1234567899", token);
-//    }
-//
-//}
+package userregister.usercore.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RefreshTokenTest {
+
+    private RefreshToken refreshToken;
+
+    @BeforeEach
+    void setUp(){
+        this.refreshToken = new RefreshToken();
+    }
+
+    @Test
+    public void checkIfWorkingWhenHappyPath() {
+
+        //when
+        String token = refreshToken.tokenCreator();
+
+        //then
+        assertEquals("1234567899", token);
+    }
+
+}
