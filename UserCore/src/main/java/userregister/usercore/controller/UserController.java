f505d7f21f90f3cdf17b405d87cbc2fa24f2ca5b
psar79 <psar1987@wp.pl>
2021-07-08 10:42:17 +0200

    LET-36 Create User Controller User Register Method and Validator

--- UserCore/src/main/java/userregister/usercore/controller/UserController.java
+++ UserCore/src/main/java/userregister/usercore/controller/UserController.java
@@ -1,18 +1,34 @@
 package userregister.usercore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
+import userregister.usercore.utils.Validator;
 
 @RestController
 public class UserController {
 
-public ResponseEntity<String> userRegister(@RequestParam String number){
-    if(Objects.isNull(number) || number.length()!=9){
-        return  null;
+    private Validator validator;
+
+    @Autowired
+    public UserController(Validator validator) {
+        this.validator = validator;
     }
-    return null;
-}
+
+    @PostMapping
+    public ResponseEntity<String> userRegister(@RequestParam String number) {
+
+        if (validator.validatePhoneNumber(number)) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Please, give proper number");
+        }
+
+
+        return null;
+
+    }
+
+
 }
--- UserCore/src/main/java/userregister/usercore/utils/Validator.java
+++ UserCore/src/main/java/userregister/usercore/utils/Validator.java
@@ -1,4 +1,15 @@
 package userregister.usercore.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
 public class Validator {
+
+    public boolean validatePhoneNumber(String number) {
+        return Objects.nonNull(number) && number.length() == 9 && number.matches("\\d{9}");
+    }
+
+
 }
