bc627dc2e43a67d7ec11cb0ee3440a6525f68b30
psar79 <psar1987@wp.pl>
2021-07-09 14:39:33 +0200

    LET-48 Create GateWayCore module

---
+++ GateWayCore/src/main/java/org/example/GateWayCore/GateWayCoreApplication.java
@@ -0,0 +1,15 @@
+package org.example.GateWayCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayCoreApplication.class, args);
+	}
+
+}
+
+
