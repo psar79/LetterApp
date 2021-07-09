15f7fa4a34b3fb62183416df38315fe43233a18f
psar79 <psar1987@wp.pl>
2021-07-09 13:50:49 +0200

    LET-47 Create endpoint for logging (return true if OK, and false if not)

--- UserCore/src/main/java/userregister/usercore/dao/entity/UserRepository.java
+++ UserCore/src/main/java/userregister/usercore/dao/entity/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByPhoneNumber(String phoneNumber);
+
 }
 
